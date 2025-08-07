9b76bc92b02bad477ba75960a7b684d1da1c312b
Y0rFa1se <neargul123@gmail.com>
2025-08-08 01:39:59 +0900

    devrun

---
+++ devrun.py
@@ -0,0 +1,27 @@
+from modules.md2html import convert_md_to_html
+from modules.file_builder import initializer
+
+import os
+
+INDEX_TAB_TITLE = "공부하다가 끄적거린거😘"
+INDEX_PAGE_TITLE = "Today I Learned 📚"
+
+def main():
+    initializer(INDEX_TAB_TITLE, INDEX_PAGE_TITLE)
+
+    for filename in os.listdir("pages"):
+        with open(os.path.join("pages", filename), "r", encoding="utf-8") as f:
+            md_content = f.read()
+            filename = filename.replace(".md", "")
+            html_content = convert_md_to_html(filename, md_content)
+
+            with open(os.path.join("docs", f"{filename}.html"), "w", encoding="utf-8") as html_file:
+                html_file.write(html_content)
+
+if __name__ == "__main__":
+    import shutil
+    shutil.rmtree("docs", ignore_errors=True)
+
+    main()
+    
+    shutil.copytree("templates/assets", "docs/assets", dirs_exist_ok=True)

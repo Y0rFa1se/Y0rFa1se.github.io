62875a3a4ff8023b5bf8a313f8b1d8d6e10dcf45
Y0rFa1se <neargul123@gmail.com>
2025-12-28 00:40:02 +0900

    donate

---
+++ src/contents/Github Pages.md
@@ -0,0 +1,5 @@
+---
+title: Github Pages
+abstract: Github Pages
+category: Computer Science/Computer Engineering/Projects/Github Pages
+---
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
     import CategoryToc from "$lib/CategoryToc.svelte";
+
+	onMount(() => {
+		if (typeof kofiWidgetOverlay !== 'undefined') {
+		kofiWidgetOverlay.draw('y0rfa1se', {
+			'type': 'floating-chat',
+			'floating-chat.donateButton.text': 'Support me',
+			'floating-chat.donateButton.background-color': '#323842',
+			'floating-chat.donateButton.text-color': '#fff'
+		});
+		}
+	});
 </script>
 
 <svelte:head>
@@ -10,6 +22,8 @@
 	<meta property="og:url" content="https://Y0rFa1se.github.io/" />
 
 	<meta name="description" content="Y0rFa1se" />
+
+	<script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
 </svelte:head>
 
 <h1>Y0rFa1se</h1>
